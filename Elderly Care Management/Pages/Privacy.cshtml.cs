ccddd48fa3f15026d42b93d6d391abface0cd67e
StarBubbel <sahasreddypatil25@gmail.com>
2023-06-21 15:10:56 -0400

    Add project files.

---
+++ Elderly Care Management/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Elderly_Care_Management.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
